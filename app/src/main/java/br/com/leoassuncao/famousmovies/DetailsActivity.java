f615a79c0be12703bcb6eceeb973e7a89c29b395
Leonardo Vinicius de Assunção S Filho <leonardo.filho@df.mi>
2018-01-15 16:30:38 -0200

    details activity layout

--- app/src/main/java/br/com/leoassuncao/famousmovies/DetailsActivity.java
+++ app/src/main/java/br/com/leoassuncao/famousmovies/DetailsActivity.java
@@ -1,8 +1,18 @@
 package br.com.leoassuncao.famousmovies;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by leonardo.filho on 15/01/2018.
  */
 
-public class DetailsActivity {
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
 }
